886e56c8001c3e500770e7c46207c810230eb826
Tanaya <53856363+tanaya137@users.noreply.github.com>
2022-08-10 00:57:00 +0300

    Update instrument_pub.js
    updated version cde

--- instrument_pub.js
+++ instrument_pub.js
@@ -12,39 +12,64 @@ client.on('connect', function () {
 
     var count = 0;
     var ds = Math.random();
-    ds = ds * 100;
-
+    ds = (ds * 100).toFixed(2);
+var random;
     function publishStableWeight() {
-        count++;
-        if (ds != 0) {
-            if (count != 5)
-                ds = (ds * 100 + 1) / 100;
+        
+      
+        
+        if(count==0)
+            {random = Math.random();
+             console.log(random+" is generated");
         }
-
-        client.publish('instrument_data', 'The weight is : ' + ds.toString() + ' gm.');
+        
+          if (ds != 0) {
+            if (count != 4)
+                ds = ((ds * 100 + 1) / 100).toFixed(2);
+        }
+        console.log("before loop random "+random);
+        if (random < 0.8000){
+            
+            
+            
+            if(count<3)
+        client.publish('instrument_data', 'Dynamic weight is : ' + ds.toString() + ' gm.');            
+            else
+        client.publish('instrument_data', 'Stable weight is : ' + ds.toString() + ' gm.');            
+        }
+        else if (random < 0.9000)
+        client.publish('instrument_data', "Command not executable.");
+        else if (random < 0.9500)
+        client.publish('instrument_data', "Underload range");
+        else if (random < 0.9900)
+        client.publish('instrument_data', "Overload range");
+        else
+        client.publish('instrument_data', 'The weight is :0 gm.');
+        
+        
         console.log(count + " is count and ds is  " + ds);
-
-        if (count == 5) {
-            count = 0;
-            var random = Math.random();
+        if (count == 4) {
+            count = -1;
+            
+            console.log("random ="+random);
             if (random < 0.8000) {
                 ds = Math.random();
-                ds = ds * 100;
+                ds = (ds * 100).toFixed(2);
             }
             else if (random < 0.9000) {
                 ds = 0;
-                client.publish('instrument_data', "Command not executable.")
             }
             else if (random < 0.9500) {
                 ds = 0;
-                client.publish('instrument_data', "Balance in Underload range.")
             }
             else if (random < 0.9900) {
                 ds = 0;
-                client.publish('instrument_data', "Balance in Overload range.");
             }
             else
                 ds = 0;
         }
+        
+        
+        count++;
     }
 })
